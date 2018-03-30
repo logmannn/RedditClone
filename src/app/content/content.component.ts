3e31a512c180e07e03407beb7c3a42e2abfd630a
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-03-30 11:12:44 -0700

    hardcode some reddit posts

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostPreview } from './models/content.model';
 
 @Component({
   selector: 'app-content',
@@ -11,5 +12,9 @@ export class ContentComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  
+  postPreviews: PostPreview[] = [
+    new PostPreview(0,'Marijuana Dispensaries Are Keeping Cannabis Out Of The Hands Of Minors In Oregon', 'https://www.forbes.com/sites/monazhang/2018/03/30/marijuana-dispensaries-are-keeping-cannabis-out-of-the-hands-of-minors-in-oregon/'),
+    new PostPreview(0,'Coast Guard', 'https://scontent-lga3-1.cdninstagram.com/vp/a32425a4a9b99972516ddbcf2b01cf4a/5B53D0CF/t51.2885-15/e35/28751440_1621525544582505_9041431883551866880_n.jpg'),
+  ];
 }
---
+++ src/app/content/models/content.model.ts
@@ -0,0 +1,4 @@
+export class PostPreview {
+  // public done: boolean = false;
+  constructor(public upvote: number, public title: string, public titleLink:string) { }
+}
