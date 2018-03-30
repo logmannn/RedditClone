38b7597998cb8dc0631ca27651e4aab41a416327
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-03-30 13:31:11 -0700

    attempt at cross component method call

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,16 +8,13 @@ import { PostPreview } from '../content/models/content.model';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(postPreviews: PostPreview[]) { }
 
   ngOnInit() {
   }
 
   submitAPost() {
-    // alert("Submit a post");
-    // PostPreview = [
-    //   new PostPreview('Marijuana Dispensaries Are Keeping Cannabis Out Of The Hands Of Minors In Oregon', 'https://www.forbes.com/sites/monazhang/2018/03/30/marijuana-dispensaries-are-keeping-cannabis-out-of-the-hands-of-minors-in-oregon/')
-    // ];
+    new PostPreview('Coast Guard', 'https://scontent-lga3-1.cdninstagram.com/vp/a32425a4a9b99972516ddbcf2b01cf4a/5B53D0CF/t51.2885-15/e35/28751440_1621525544582505_9041431883551866880_n.jpg');
     alert(PostPreview.length)
   }
 
