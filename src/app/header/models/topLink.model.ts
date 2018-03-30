e46758f44736388ee5117027ff9eecfebc74afdf
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-03-30 10:19:51 -0700

    come up what comprises a reddit and a subreddit

--- src/app/header/models/topLink.model.ts
+++ src/app/header/models/topLink.model.ts
@@ -2,7 +2,3 @@ export class TopLink {
   // public done: boolean = false;
   constructor(public link: string, public linkText: string) { }
 }
-// TopLinks: TopLink[] = [
-//   new TopLink('https://www.reddit.com/r/popular/', 'popular'),
-//   new TopLink('https://www.reddit.com/r/all/', 'all')
-// ];
