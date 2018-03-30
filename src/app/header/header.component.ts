5282777c5a152961bb983dbddd34c65eb91806e8
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-03-30 10:00:43 -0700

    add a model of links for header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TopLink } from './models/topLink.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+  topLinks: TopLink[] = [
+    new TopLink('https://www.reddit.com/r/popular/', 'popular'),
+    new TopLink('https://www.reddit.com/r/all/', 'all')
+  ];
 
 }
---
+++ src/app/header/models/topLink.model.ts
@@ -0,0 +1,8 @@
+export class TopLink {
+  // public done: boolean = false;
+  constructor(public link: string, public linkText: string) { }
+}
+// TopLinks: TopLink[] = [
+//   new TopLink('https://www.reddit.com/r/popular/', 'popular'),
+//   new TopLink('https://www.reddit.com/r/all/', 'all')
+// ];
