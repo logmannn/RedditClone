9856d9fdf68e2c179df8553d28c7d9e3f979337c
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-03-30 14:16:38 -0700

    got test post to submit

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -22,9 +22,9 @@ export class ContentComponent implements OnInit {
   ];
 
   submitAPost() {
-    // new PostPreview('Coast Guard', 'https://scontent-lga3-1.cdninstagram.com/vp/a32425a4a9b99972516ddbcf2b01cf4a/5B53D0CF/t51.2885-15/e35/28751440_1621525544582505_9041431883551866880_n.jpg');
-    console.log(this.postPreviews);
-    
+    //get this information from a popup form
+    this.postPreviews.push({upvote: 0, title: 'Marijuana Dispensaries Are Keeping Cannabis Out Of The Hands Of Minors In Oregon', titleLink: 'https://www.forbes.com/sites/monazhang/2018/03/30/marijuana-dispensaries-are-keeping-cannabis-out-of-the-hands-of-minors-in-oregon/'});
+    console.log(PostPreview);
   }
 
 }
